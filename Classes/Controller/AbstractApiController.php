3cc5b06b1f3b5b2c898806d190852c26b5f4682b
Serhii Borulko <borulkosergey@icloud.com>
2018-11-20 13:12:31 +0200

    Introduce Basic API Controller for future usage

---
+++ Classes/Controller/AbstractApiController.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types = 1);
+
+namespace LMS3\Support\Controller;
+
+/* * *************************************************************
+ *
+ *  Copyright notice
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * @author Sergey Borulko <borulkosergey@icloud.com>
+ */
+abstract class AbstractApiController extends Base\ApiController
+{
+    /**
+     * Just render all the existing items related to specific resource
+     */
+    public function listAction(): void
+    {
+        $this->checkAccess();
+
+        $this->view->setVariablesToRender([$this->getRootName()]);
+
+        $this->view->assign($this->getRootName(), $this->getResourceRepository()->findAll());
+    }
+
+    /**
+     * @param \TYPO3\CMS\Extbase\DomainObject\AbstractEntity $entity
+     * @param array                                          $data
+     */
+    public function editAction(AbstractEntity $entity, array $data): void
+    {
+        $this->checkAccess();
+
+        $this->view->setVariablesToRender([$this->getRootName()]);
+
+        foreach ($data as $propertyName => $propertyValue) {
+            $entity->_setProperty($propertyName, $propertyValue);
+        }
+
+        $this->view->assign($this->getRootName(), $this->getResourceRepository()->upgrade($entity));
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return string
+     */
+    public function createAction(array $data): string
+    {
+        $this->checkAccess();
+
+        $repository = $this->getResourceRepository();
+
+        return json_encode([
+            'success' => $repository->persist($repository->produce($data))
+        ]);
+    }
+
+    /**
+     * @param \TYPO3\CMS\Extbase\DomainObject\AbstractEntity $entity
+     *
+     * @return string
+     */
+    public function destroyAction(AbstractEntity $entity): string
+    {
+        $this->checkAccess();
+
+        return json_encode([
+            'success' => $this->getResourceRepository()->destroy($entity)
+        ]);
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function failAction(string $message): ResponseInterface
+    {
+        return self::createWith($message);
+    }
+}
---
+++ Classes/Controller/Base/ApiController.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types = 1);
+
+namespace LMS3\Support\Controller\Base;
+
+/* * *************************************************************
+ *
+ *  Copyright notice
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+use LMS3\Support\Extbase\{User, Response, Action\CouldReturnPsrResponse};
+use TYPO3\CMS\Extbase\Persistence\RepositoryInterface;
+
+/**
+ * @author Sergey Borulko <borulkosergey@icloud.com>
+ */
+abstract class ApiController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+    use User, CouldReturnPsrResponse, Response;
+
+    /**
+     * @var \TYPO3\CMS\Extbase\Mvc\View\JsonView
+     */
+    public $view;
+
+    /**
+     * Deny the request if user is not logged in or denied by endpoint
+     * InitializeAction will not work in that case, because we need to use forward
+     */
+    public function checkAccess(): void
+    {
+        if (User::isNotLoggedIn() || !$this->isAllowed($this->request->getArguments())) {
+            $this->forward('fail', null, null, ['message' => 'Access denied']);
+        }
+    }
+
+    /**
+     * Should return the Repository of the Resource
+     *
+     * @return \LMS3\Support\Domain\Repository\AbstractRepository
+     */
+    abstract protected function getResourceRepository(): RepositoryInterface;
+
+    /**
+     * @see setVariablesToRender()
+     * @return string
+     */
+    abstract protected function getRootName(): string;
+
+    /**
+     * @param array $requestArguments
+     *
+     * @return bool
+     */
+    abstract protected function isAllowed(array $requestArguments = []): bool;
+}
