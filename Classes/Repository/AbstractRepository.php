df01938a18399b36526dc39d807213e49454c8d8
Serhii Borulko <borulkosergey@icloud.com>
2018-11-20 14:05:07 +0200

    Fix typo in the Repository namespace

--- Classes/Repository/AbstractRepository.php
+++ Classes/Repository/AbstractRepository.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace LMS3\Support\Domain\Repository;
+namespace LMS3\Support\Repository;
 
 /* * *************************************************************
  *
